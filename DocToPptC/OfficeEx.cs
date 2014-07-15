8aada8ff60af1999ec179e488f7065b71c069d1f
avhimkov <avhimkov@mail.ru>
2014-07-15 14:13:58 +0600

    to commet

---
+++ DocToPptC/OfficeEx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using P = DocumentFormat.OpenXml.Presentation;
+using Slide = DocumentFormat.OpenXml.Presentation.Slide;
+using ODD = DocumentFormat.OpenXml.Wordprocessing;
+using ODP = DocumentFormat.OpenXml.Drawing;
+using Shape = DocumentFormat.OpenXml.Presentation.Shape;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Linq;
+
+namespace DocToPptC
+{
+    public class OfficeEx
+    {
+        public static string DocxFile = @"D:\DocToPPTС\Doc.docx";
+        public static string PptxFile = @"D:\DocToPPTС\Presentation1.pptx";
+        public static string ExcelFile1 = @"D:\DocToPPTС\Presentation1.xlsx";
+        public static string ExcelFile2 = @"D:\DocToPPTС\Справка  СиС.xlsx";
+     
+        private static string _txt = "";
+        private static string _word = "";
+
+        public static string Word
+        {
+            get { return _word; }
+            set { _word = value; }
+        }
+        public static string Txt
+        {
+            get { return _txt; }
+            set { _txt = value; }
+        }
+
+        public static string ReadWordIp(int index)
+        {
+            /*Разбиваем параграф из DocGetPar на слова с индексом*/
+            string[] words = Txt.Split(new[] { ' ', ',', ':', '?', '!', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            string Word = words[index];
+            return Word;
+        }
+        public static string ExcelGetVal(string filepatch, int indexWSP, int indexSheetD, int indexRow, int indexCell)
+        {
+            /*ставим указатель на место вставки в таблице*/
+            using (SpreadsheetDocument docExcel = SpreadsheetDocument.Open(filepatch, true))
+            {
+                WorkbookPart workbookPart = docExcel.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.ElementAt(indexWSP);
+                SheetData sheetData = worksheetPart.Worksheet.ChildElements.OfType<SheetData>().ElementAt(indexSheetD);
+                Row row1 = sheetData.ChildElements.OfType<Row>().ElementAt(indexRow);
+                Cell cell1 = row1.ChildElements.OfType<Cell>().ElementAt(indexCell);
+                CellValue cellValue1 = cell1.GetFirstChild<CellValue>();
+                if (cellValue1.InnerText != "")
+                {
+                    Txt = cellValue1.InnerText;
+                }
+            }
+            return Txt;
+        }
+
+        public static void PptxGetTab(string filepatch, int indexSlide, int indexGraphicFrame, int indexRow, int indexCell, int indexRun)
+        /*ставим указатель на место вставки текста в таблицу*/
+        {
+            using (PresentationDocument prstDoc = PresentationDocument.Open(filepatch, true))
+            {
+                Slide firstSlide = prstDoc.PresentationPart.SlideParts.ElementAt(indexSlide).Slide;
+                P.CommonSlideData commonSlideData1 = firstSlide.GetFirstChild<P.CommonSlideData>();
+                P.ShapeTree shapeTree1 = commonSlideData1.GetFirstChild<P.ShapeTree>();
+                P.GraphicFrame graphicFrame2 = shapeTree1.Elements<P.GraphicFrame>().ElementAt(indexGraphicFrame);
+                ODP.Graphic graphic2 = graphicFrame2.GetFirstChild<ODP.Graphic>();
+                ODP.GraphicData graphicData2 = graphic2.GetFirstChild<ODP.GraphicData>();
+                ODP.Table table1 = graphicData2.GetFirstChild<ODP.Table>();
+                ODP.TableRow tableRow1 = table1.Elements<ODP.TableRow>().ElementAt(indexRow);
+                ODP.TableCell tableCell1 = tableRow1.Elements<ODP.TableCell>().ElementAt(indexCell);
+                ODP.TextBody textBody1 = tableCell1.GetFirstChild<ODP.TextBody>();
+                ODP.Paragraph paragraph1 = textBody1.GetFirstChild<ODP.Paragraph>();
+                ODP.Run run1 = paragraph1.ChildElements.OfType<ODP.Run>().ElementAt(indexRun);
+                ODP.Text text1 = run1.GetFirstChild<ODP.Text>();
+                text1.Text = Txt;
+              }
+        }
+
+        public static void PptxGetPar(string filepatch, int indexslide, int indexshape, int indexpara, int indexrun)
+        /*ставим указатель на место вставки текста*/
+        {
+            using (PresentationDocument prstDoc = PresentationDocument.Open(filepatch, true))
+            {
+                Slide firstSlide = prstDoc.PresentationPart.SlideParts.ElementAt(indexslide).Slide;
+                Shape firstShape = firstSlide.CommonSlideData.ShapeTree.ChildElements.OfType<Shape>().ElementAt(indexshape);
+                ODP.Paragraph paraP = firstShape.TextBody.ChildElements.OfType<ODP.Paragraph>().ElementAt(indexpara);
+                ODP.Text t = paraP.ChildElements.OfType<ODP.Run>().ElementAt(indexrun).Text;
+                if (paraP.InnerText != "")
+                {
+                    Txt = paraP.InnerText;
+                }
+                t.Text = Txt;
+                prstDoc.PresentationPart.Presentation.Save();
+            }
+        }
+
+        public static string DocGetPar(string filepatch, int indexparagraf, int indexrun, int indextext)
+        /*Берем параграфы и слова из DOCX и возвращаем текст*/
+        {
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filepatch, true))
+            {
+                ODD.Body body = wordDoc.MainDocumentPart.Document.Body;
+                ODD.Paragraph para = body.ChildElements.OfType<ODD.Paragraph>().ElementAt(indexparagraf);
+                ODD.Run run = para.ChildElements.OfType<ODD.Run>().ElementAt(indexrun);
+                ODD.Text t = run.ChildElements.OfType<ODD.Text>().ElementAt(indextext);
+                if (para.InnerText != "")
+                {
+                    Txt = para.InnerText;
+                }
+            }
+            return Txt;
+        } 
+    }
+}
---
+++ DocToPptC/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using P = DocumentFormat.OpenXml.Presentation;
+using ODD = DocumentFormat.OpenXml.Wordprocessing;
+using ODP = DocumentFormat.OpenXml.Drawing;
+
+namespace DocToPptC
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           /*Вводная параграф*/
+            OfficeEx.Word = OfficeEx.DocGetPar(OfficeEx.DocxFile, 6, 0, 0);
+            
+            OfficeEx.PptxGetPar(OfficeEx.PptxFile, 0, 1, 0, 0);
+
+            /*заголовок TODO*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 5, 0, 0);
+//            OfficeEx.Word = OfficeEx.ReadWordIp(5);
+//            OfficeEx.Txt = "";
+//            OfficeEx.PptxGetPar(OfficeEx.PptxFile, 0, 1, 0, 0);
+         
+           /*температура*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 6, 0, 0);
+//            OfficeEx.Word = OfficeEx.ReadWordIp(2);
+//            OfficeEx.Txt = OfficeEx.Word;
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 1, 1, 1, 0);
+
+            /*осадки*/
+/*            string osadB = OfficeEx.ReadWordIp(9);
+              OfficeEx.Word = osadA + " " + osadB;*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 6, 0, 0);
+//            string osadA = OfficeEx.ReadWordIp(8);
+//            OfficeEx.Word = osadA;
+//            OfficeEx.Txt = OfficeEx.Word;
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 1, 2, 1, 0); 
+
+            /*Направление и скорость ветра*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 6, 0, 0);
+//            string veterA = OfficeEx.ReadWordIp(5).ToUpper().Remove(1, 5).Remove(3, 7);
+//            string veterB = OfficeEx.ReadWordIp(6);
+//            string veterC = OfficeEx.ReadWordIp(7);
+//            OfficeEx.Word = veterA + " " + veterB + " " + veterC;
+//            OfficeEx.Txt = OfficeEx.Word.Remove(9, 2);
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 1, 3, 1, 0);
+
+            /*Пострадало*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 5, 0, 0);
+//            string gervA = OfficeEx.ReadWordIp(53);
+//            OfficeEx.Word = gervA;
+//            OfficeEx.Txt = OfficeEx.Word;
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 3, 1, 1, 0);
+
+            /*Погибло*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 5, 0, 0);
+//            string gibelA = OfficeEx.ReadWordIp(57);
+//            OfficeEx.Word = gibelA;
+//            OfficeEx.Txt = OfficeEx.Word;
+//            Console.WriteLine(Offic
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 3, 2, 1, 0);
+
+            /*Госпитализированно*/
+//            OfficeEx.Txt = OfficeEx.DocGetPar(OfficeEx.DocxFile, 5, 0, 0);
+//            string gospitA = OfficeEx.ReadWordIp(60);
+//            OfficeEx.Word = gospitA;
+//            OfficeEx.Txt = OfficeEx.Word;
+//            Console.WriteLine
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 3, 3, 1, 0);
+
+
+
+            /*От МЧС л/с, чел.*/
+//            OfficeEx.Txt = OfficeEx.ExcelGetVal(OfficeEx.ExcelFile1, 0, 0, 8, 2);
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 4, 3, 1, 0);
+
+            /*От МЧС тех.ед*/
+//            OfficeEx.Txt = OfficeEx.ExcelGetVal(OfficeEx.ExcelFile1, 0, 0, 8, 3);
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 4, 3, 2, 0);
+
+            /*Всего л/с, чел.*/
+//            OfficeEx.Txt = OfficeEx.ExcelGetVal(OfficeEx.ExcelFile1, 0, 0, 20, 2);
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 4, 4, 1, 0);
+
+            /*Всего тех.ед*/
+//            OfficeEx.Txt = OfficeEx.ExcelGetVal(OfficeEx.ExcelFile1, 0, 0, 20, 3);
+//            OfficeEx.PptxGetTab(OfficeEx.PptxFile, 0, 4, 4, 2, 0);
+
+
+
+        }
+
+     
+    }
+}
